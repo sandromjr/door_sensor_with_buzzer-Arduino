498aa68ef28dfc9f2fe396aaaedc953051bb82ad
Sandro Junior <60121783+sandromjr@users.noreply.github.com>
2020-06-08 00:27:34 -0300

    comments

--- door_sensor.ino
+++ door_sensor.ino
@@ -1,27 +1,36 @@
 unsigned long millist1 = millis();
 unsigned long millist2 = millis();
-const int led1 = 13;
-const int led2 = 12;
-const int redled = 7;
-const int greenled = 6;
-const int switch1 = 9;
-const int switch2 = 8;
+const int led1 = 13; //Left door led
+const int led2 = 12; //Right door led
+const int redled = 7; //Indicator if button is OFF
+const int greenled = 6; //Indicator if button is ON
+const int switch1 = 9; //Left door reed switch
+const int switch2 = 8; //Right door reed switch
 const int buzz = 2;
-const int button = 0;
+const int button = 0; //ON/OFF switch button
 int status1 = 0;
 int status2 = 0;
 int statusbutton = 0;
 
 void setup() {
-
+//Define inputs and output pins
+    
     pinMode(led1, OUTPUT);
-    pinMode(but1, INPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(greenled, OUTPUT);
+    pinMode(redled, OUTPUT);
+    pinMode(switch1, INPUT);
+    pinMode(switch2, INPUT);
+    pinMode(button, INPUT);
     
 }
 
 void loop() {
+//Read logic signal in 0
   
     statusbutton = digitalRead(button);
+    
+//If button is pressed (ON), turn off red indicator, turn on green indicador and call both functions
 
     if (statusbutton == HIGH){
         digitalWrite(greenled,HIGH);
@@ -29,6 +38,8 @@ void loop() {
         door1();
         door2();
     }
+
+//If button pressed is OFF, turn off green led and turn on red led
     else {
         digitalWrite(redled,HIGH);
         digitalWrite(greenled,LOW);
@@ -36,12 +47,15 @@ void loop() {
 }
 
 void door1() {
-  
+//Verify if door is opened (megnet isn't close to the reed switch, that indicates LOW logical signal in 9)
+    
     status1 = digitalRead(switch1);
 
     if (status1 == LOW) {
-        digitalWrite(led1,HIGH);
+        digitalWrite(led1,HIGH); //Left led is turned on
         if ((millis() - millist1) > 120000) {
+//If the left door is opened for more than 2 minutes, the buzzer will bip
+            
             tone(buzz,5000, 100);
             delay(300);
             tone(buzz,5000, 100);
@@ -56,12 +70,15 @@ void door1() {
 }
 
 void door2() {
+//Verify if door is opened (megnet isn't close to the reed switch, that indicates LOW logical signal in 8)
   
     status2 = digitalRead(switch2);
 
     if (status2 == LOW) {
-        digitalWrite(led2,HIGH);
+        digitalWrite(led2,HIGH); //Right led is turned on
         if ((millis() - millist2) > 120000) {
+//If the right door is opened for more than 2 minutes, the buzzer will bip
+            
             tone(buzz,5000, 100);
             delay(300);
             tone(buzz,5000, 100);
